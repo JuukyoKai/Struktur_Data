548d9a86a120f8228dcc99765fc8f34ba5a34512
JuukyoKai <juukyokai00@gmail.com>
2020-05-11 16:13:02 +0700

    update Graph

--- Graph.cpp
+++ Graph.cpp
@@ -39,11 +39,11 @@ class Graph{
 			}
 			else return false;
 		}
-		int cariIndex(int key){
+		vector<int>::iterator cariIndex(int key){
 			vector<int> vkey;
 			vkey.push_back(key);
-			vector<int>::iterator it=std::find_end(vertexArr.begin(), vertexArr.end(), vkey.begin(), vkey.end());
-			return std::distance(vertexArr.begin(), it);
+			vector<int>::iterator it=std::search(vertexArr.begin(), vertexArr.end(), vkey.begin(), vkey.end());
+			return it;
 		}
 		
 		/*           Mandatory function              */
@@ -68,12 +68,13 @@ class Graph{
 			vertexArr.push_back(key);
 			vertexPtrArr.push_back(tempVert);
 		}
+		
 		void addEdge(int fromVert, int toVert){
 			//make temporary object
 			nodeEdge *tempEdge = new nodeEdge();
 			//search and save index vertex
-			int indexFrom = cariIndex(fromVert);
-			int indexTo = cariIndex(toVert);
+			int indexFrom = std::distance(vertexArr.begin(), cariIndex(fromVert));
+			int indexTo = std::distance(vertexArr.begin(), cariIndex(toVert));
 			//connecting between vertexes
 			vertexPtrArr[indexFrom]->setConnectedEdge(tempEdge);
 			vertexPtrArr[indexFrom]->setConnectedVertex(vertexPtrArr[indexTo]);
@@ -81,15 +82,13 @@ class Graph{
 			vertexPtrArr[indexTo]->setConnectedVertex(vertexPtrArr[indexFrom]);
 			
 			//cout << vertexPtrArr[indexFrom] << endl;
-			
-			
 		}
 		void addEdge(int fromVert, int toVert, int weight){
 			//make temporary object
 			nodeEdge *tempEdge = new nodeEdge(weight);
 			//search and save index vertex
-			int indexFrom = cariIndex(fromVert);
-			int indexTo = cariIndex(toVert);
+			int indexFrom = std::distance(vertexArr.begin(), cariIndex(fromVert));
+			int indexTo = std::distance(vertexArr.begin(), cariIndex(toVert));
 			//connecting between vertexes
 			vertexPtrArr[indexFrom]->setConnectedEdge(tempEdge);
 			vertexPtrArr[indexFrom]->setConnectedVertex(vertexPtrArr[indexTo]);
@@ -97,14 +96,32 @@ class Graph{
 			vertexPtrArr[indexTo]->setConnectedVertex(vertexPtrArr[indexFrom]);
 			
 			//cout << vertexPtrArr[indexFrom] << endl;
-			
-			
 		}
+		void getVertex(int vertKey){
+			if(cariIndex(vertKey) <= vertexArr.end()){
+				cout << "vertex yang anda cari adalah : " << vertKey << " Status : Tidak Ditemukan" << endl;	
+			} else {
+				cout << "vertex yang anda cari adalah : " << vertKey << " Status : Ditemukan" << endl;
+			}
+		}
+		
+		getVertices(){
+			vector<int>::iterator it;
+			for (it = vertexArr.begin(); it != vertexArr.end(); it++) { 
+           		cout << *it << ' '; 
+        	} 
+		}
+		
+		
+		
 		void test(int t){
-			int indexTest = cariIndex(t);
+			int indexTest = std::distance(vertexArr.begin(), cariIndex(t));
 			nodeVertex *tempVert = new nodeVertex(t);
 			cout << "Node : " << t << " Index : " << NULL << " Ptr : "<< tempVert << endl;
 			cout << "Node : " << t << " Index : " << indexTest << " Ptr : "<< vertexPtrArr[indexTest] << endl;
+		}
+		void printEdge(){
+			
 		}
 };
 
@@ -117,6 +134,7 @@ int main(int argc, char** argv) {
 	myGraph.test(1);
 	myGraph.test(2);
 	myGraph.addEdge(1,2);
+	myGraph.getVertex(4);
 	
 	
 	return 0;
